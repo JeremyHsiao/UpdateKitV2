9e51c051d62cdba49b70d8e1baa2ba558133683d
Jeremy Hsiao <jeremy.hsiao@outlook.com>
2020-08-20 11:09:16 +0800

    Update fw_version.h
    Set FW version to 1.3 from now on.

--- src/fw_version.h
+++ src/fw_version.h
@@ -8,7 +8,7 @@
 #ifndef FW_VERSION_H_
 #define FW_VERSION_H_
 
-#define FW_STRING   "1.2"
+#define FW_STRING   "1.3"
 #define FW_MAJOR    (FW_STRING[0])
 #define FW_MIDDLE   (FW_STRING[1])
 #define FW_MINOR    (FW_STRING[2])
